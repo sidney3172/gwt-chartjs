1c4baae86a161f006d9ed5efaf62153b98729dbd
Sergey Gibert <sergey.gibert@gmail.com>
2014-08-10 14:39:49 +0400

    Added getting started info

--- gwt-chartjs-doc/src/main/java/io/github/sidney3172/doc/client/start/GettingStartedView.java
+++ gwt-chartjs-doc/src/main/java/io/github/sidney3172/doc/client/start/GettingStartedView.java
@@ -1,7 +1,9 @@
 package io.github.sidney3172.doc.client.start;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.TextAreaElement;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -14,7 +16,22 @@ public class GettingStartedView extends Composite{
 			UiBinder<Widget, GettingStartedView> {
 	}
 
+    @UiField
+    TextAreaElement dependencyCode;
+    @UiField
+    TextAreaElement inheritCode;
+
 	public GettingStartedView() {
 		initWidget(uiBinder.createAndBindUi(this));
+        StringBuilder builder = new StringBuilder();
+        builder.append("<dependency>\n");
+        builder.append("\t<groupId>io.github.sidney3172</groupId>\n");
+        builder.append("\t <artifactId>gwt-chartjs</artifactId>\n");
+        builder.append("\t <version>0.1.1</version>\n");
+        builder.append("<dependency>");
+
+        dependencyCode.setInnerText(builder.toString());
+
+        inheritCode.setInnerText("<inherits name=\"io.github.sidney3172.ChartJs\"/>");
 	}
 }
